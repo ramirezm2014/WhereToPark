9ec096d99597147fa8b485d14076f4f37774775c
amandalyman <smart1intheclass95@yahoo.com>
2017-01-17 10:39:52 -0500

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,5 +1,11 @@
 function initMap()
 {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 26.370653, lng: -80.102302},
+		zoom: 15,
+		disableDefaultUI: true
+	});
+	
 	var map_design = new google.maps.StyledMapType(
 		[
 			{
@@ -105,13 +111,45 @@ function initMap()
 		],
 		{name: 'Custom Map'}
 	);
-
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 26.370653, lng: -80.102302},
-		zoom: 15,
-		disableDefaultUI: true
-	});
-
 	map.mapTypes.set('styled_map', map_design);
 	map.setMapTypeId('styled_map');
+
+	var lot1Blue = new google.maps.Polygon({
+		paths: [
+			{lat: 26.371600, lng: -80.099704},
+			{lat: 26.371600, lng: -80.098824},
+			{lat: 26.371505, lng: -80.098824},
+			{lat: 26.371189, lng: -80.098795},		
+			{lat: 26.369740, lng: -80.098783},
+			{lat: 26.369740, lng: -80.099180},
+			{lat: 26.370075, lng: -80.099180},	
+			{lat: 26.370075, lng: -80.099373},
+			{lat: 26.370680, lng: -80.099373},	
+			{lat: 26.370680, lng: -80.099180},
+			{lat: 26.371500, lng: -80.099180},
+			{lat: 26.371500, lng: -80.099704},
+			{lat: 26.371600, lng: -80.099704}],
+		strokeColor: 'blue',
+		strokeOpacity: 0.8,
+		strokeWeight: 2,
+		fillColor: 'blue',
+		fillOpacity: 0.35 //This opacity will be to determine availability
+	});
+	lot1Blue.setMap(map);
+	
+	var lot1Red = new google.maps.Polygon({
+		paths: [
+			{lat: 26.371498, lng: -80.099629},
+			{lat: 26.371498, lng: -80.099182},
+			{lat: 26.370682, lng: -80.099182},
+			{lat: 26.370682, lng: -80.099629},
+			{lat: 26.371498, lng: -80.099629}
+		],
+		strokeColor: 'red',
+		strokeOpacity: 0.8,
+		strokeWeight: 2,
+		fillColor: 'red',
+		fillOpacity: 0.35 //This opacity will be to determine availability
+	});
+	lot1Red.setMap(map);
 }
